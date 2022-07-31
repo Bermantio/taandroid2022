a71b446c79586bc4c170d2b6f13e77b3d811cf6c
bermantio <bermantiosugiharto@gmail.com>
2022-08-01 01:29:47 +0700

    login DATABASE oke, regis error

--- app/src/main/java/com/example/lazismu/Login.java
+++ app/src/main/java/com/example/lazismu/Login.java
@@ -1,11 +1,13 @@
 package com.example.lazismu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -13,17 +15,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
 
     TextView daftar, bantuan;
     Button masuk;
-    EditText username, password;
+    EditText inputemail, inputpassword;
+    String email, password;
+    private FirebaseAuth mAuth;
     boolean passwordVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
 
         daftar = (TextView)findViewById(R.id.buatakunbaru);
         daftar.setOnClickListener(new View.OnClickListener() {
@@ -41,32 +52,32 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.katasandi);
+        inputemail = (EditText) findViewById(R.id.email);
+        inputpassword = (EditText) findViewById(R.id.katasandi);
 
-        password.setOnTouchListener(new View.OnTouchListener() {
+        inputpassword.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 final int Right=2;
                 if(event.getAction()==MotionEvent.ACTION_UP){
-                    if(event.getRawX()>=password.getRight()-password.getCompoundDrawables()[Right].getBounds().width())
+                    if(event.getRawX()>=inputpassword.getRight()-inputpassword.getCompoundDrawables()[Right].getBounds().width())
                     {
-                        int selection=password.getSelectionEnd();
+                        int selection=inputpassword.getSelectionEnd();
                         if(passwordVisible){
                             //set drawable image here
-                            password.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_off,0);
+                            inputpassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_off,0);
                             //for hide password
-                            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            inputpassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
                             passwordVisible=false;
                         }
                         else{
                             //set drawable image here
-                            password.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_on,0);
+                            inputpassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_on,0);
                             //for hide password
-                            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            inputpassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
                             passwordVisible=true;
                         }
-                        password.setSelection(selection);
+                        inputpassword.setSelection(selection);
                         return true;
                     }
                 }
@@ -75,19 +86,30 @@ public class Login extends AppCompatActivity {
         });
 
         masuk = (Button) findViewById(R.id.masuk);
-        String user="admin";
-        String pass="admin";
         masuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (username.getText().toString().equalsIgnoreCase(user) && password.getText().toString().equalsIgnoreCase(pass)){
-                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                }
-                else {
-                    //jika login gagal
-                    Toast.makeText(Login.this,"Username atau Password Anda salah!",Toast.LENGTH_SHORT).show();
-                }
+                cekLogin();
             }
         });
+    }
+
+    private void cekLogin() {
+        email = inputemail.getText().toString();
+        password = inputpassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Login.this,"Username atau Password Anda salah!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/lazismu/Register.java
+++ app/src/main/java/com/example/lazismu/Register.java
@@ -1,5 +1,6 @@
 package com.example.lazismu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -18,11 +19,18 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Register extends AppCompatActivity {
 
-    EditText username, password1, password2, namapengguna, namalengkap, password, profesi;
+    EditText username, inputpassword1, inputpassword2, namapengguna, namalengkap, alamat, inputemail, no, profesi;
+    String email, password1, password2;
     Button daftar, batal;
     Spinner spinner;
+    private FirebaseAuth mAuth;
     ArrayAdapter<CharSequence> adapter;
     boolean passwordVisible;
 
@@ -31,30 +39,33 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        password1 = (EditText) findViewById(R.id.txtpassword);
-        password1.setOnTouchListener(new View.OnTouchListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        inputemail = (EditText) findViewById(R.id.email);
+        inputpassword1 = (EditText) findViewById(R.id.txtpassword);
+        inputpassword1.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 final int Right=2;
                 if(event.getAction()==MotionEvent.ACTION_UP){
-                    if(event.getRawX()>=password1.getRight()-password1.getCompoundDrawables()[Right].getBounds().width())
+                    if(event.getRawX()>=inputpassword1.getRight()-inputpassword1.getCompoundDrawables()[Right].getBounds().width())
                     {
-                        int selection=password1.getSelectionEnd();
+                        int selection=inputpassword1.getSelectionEnd();
                         if(passwordVisible){
                             //set drawable image here
-                            password1.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_off,0);
+                            inputpassword1.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_off,0);
                             //for hide password
-                            password1.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            inputpassword1.setTransformationMethod(PasswordTransformationMethod.getInstance());
                             passwordVisible=false;
                         }
                         else{
                             //set drawable image here
-                            password1.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_on,0);
+                            inputpassword1.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_on,0);
                             //for hide password
-                            password1.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            inputpassword1.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
                             passwordVisible=true;
                         }
-                        password1.setSelection(selection);
+                        inputpassword1.setSelection(selection);
                         return true;
                     }
                 }
@@ -62,30 +73,30 @@ public class Register extends AppCompatActivity {
             }
         });
 
-        password2 = (EditText) findViewById(R.id.txtulangipassword);
-        password2.setOnTouchListener(new View.OnTouchListener() {
+        inputpassword2 = (EditText) findViewById(R.id.txtulangipassword);
+        inputpassword2.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 final int Right=2;
                 if(event.getAction()==MotionEvent.ACTION_UP){
-                    if(event.getRawX()>=password2.getRight()-password2.getCompoundDrawables()[Right].getBounds().width())
+                    if(event.getRawX()>=inputpassword2.getRight()-inputpassword2.getCompoundDrawables()[Right].getBounds().width())
                     {
-                        int selection=password2.getSelectionEnd();
+                        int selection=inputpassword2.getSelectionEnd();
                         if(passwordVisible){
                             //set drawable image here
-                            password2.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_off,0);
+                            inputpassword2.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_off,0);
                             //for hide password
-                            password2.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            inputpassword2.setTransformationMethod(PasswordTransformationMethod.getInstance());
                             passwordVisible=false;
                         }
                         else{
                             //set drawable image here
-                            password2.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_on,0);
+                            inputpassword2.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.visibility_on,0);
                             //for hide password
-                            password2.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            inputpassword2.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
                             passwordVisible=true;
                         }
-                        password2.setSelection(selection);
+                        inputpassword2.setSelection(selection);
                         return true;
                     }
                 }
@@ -98,6 +109,14 @@ public class Register extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        daftar = (Button) findViewById(R.id.daftar);
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrasi();
+            }
+        });
+
         batal = (Button) findViewById(R.id.batal);
         batal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,5 +124,25 @@ public class Register extends AppCompatActivity {
                 startActivity(new Intent(Register.this, Login.class));
             }
         });
+    }
+
+    private void registrasi() {
+        email = inputemail.getText().toString();
+        password1 = inputpassword1.getText().toString();
+        password2 = inputpassword2.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email,password2)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Register.this,"Username atau Password Anda salah!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
