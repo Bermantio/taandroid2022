10875ed5d4604b3b3bb454839913389041734450
bermantio <bermantiosugiharto@gmail.com>
2022-08-02 10:17:05 +0700

    regis error, belum ada arus ke database, front hampir oke

---
+++ app/src/main/java/com/example/lazismu/FormZakatFitrah.java
@@ -0,0 +1,58 @@
+package com.example.lazismu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class FormZakatFitrah extends AppCompatActivity {
+
+    ImageView batalarah;
+    Button batalbutton;
+    Spinner txtberupa;
+    ArrayAdapter<CharSequence> adapter;
+    TextView txttanggaltransaksi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_zakat_fitrah);
+
+        txttanggaltransaksi = (TextView)findViewById(R.id.txttanggaltransaksi);
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateTanggal = new SimpleDateFormat("dd MMMM yyyy");
+        String dateTanggal = simpleDateTanggal.format(calendar.getTime());
+        txttanggaltransaksi.setText(dateTanggal);
+
+        txtberupa = (Spinner) findViewById(R.id.txtberupa);
+        adapter = ArrayAdapter.createFromResource(this, R.array.txtberupa, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        txtberupa.setAdapter(adapter);
+
+        batalbutton = (Button) findViewById(R.id.batal);
+        batalbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), FormZakat.class));
+            }
+        });
+
+        batalarah = (ImageView) findViewById(R.id.kembalizakatfitrah);
+        batalarah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), FormZakat.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/lazismu/Laporan.java
@@ -0,0 +1,64 @@
+package com.example.lazismu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Laporan extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    TextView txtjam, txttanggal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laporan);
+
+        txttanggal = (TextView)findViewById(R.id.txttanggal);
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateTanggal = new SimpleDateFormat("dd MMMM yyyy");
+        String dateTanggal = simpleDateTanggal.format(calendar.getTime());
+        txttanggal.setText(dateTanggal);
+
+        txtjam = (TextView)findViewById(R.id.txtjam);
+        Calendar jam = Calendar.getInstance();
+        SimpleDateFormat simpleDateTime = new SimpleDateFormat("hh:mm a");
+        String dateTime = simpleDateTime.format(jam.getTime());
+        txtjam.setText(dateTime);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomnavigator);
+        bottomNavigationView.setSelectedItemId(R.id.laporannavigasi);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.laporannavigasi:
+                        startActivity(new Intent(getApplicationContext(),Laporan.class));
+                        break;
+                    case R.id.beranda:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        break;
+                    case R.id.riwayatnavigasi:
+                        startActivity(new Intent(getApplicationContext(), Riwayat.class));
+                        break;
+                    case R.id.profilnavigasi:
+                        startActivity(new Intent(getApplicationContext(), Profil.class));
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+}
